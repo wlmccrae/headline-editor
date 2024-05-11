282035cbb1ecf94f58399f99cf3f0ccfa7c2b813
Wanda McCrae <wanda.mccrae@gmail.com>
2024-05-11 14:09:51 -0400

    Takes user input for month and year to ssearch archive

--- ghi/src/components/MainPage.js
+++ ghi/src/components/MainPage.js
@@ -2,17 +2,43 @@ import React, {useState} from 'react';
 
 function MainPage() {
     const [copyright, setCopyright] = useState('');
-    const [articleLoaded, setArticleLoaded] = useState(false);
+    const [resultsLoaded, setResultsLoaded] = useState(false);
     const [myArticle, setMyArticle] = useState({
         headline: {main: ''},
         snippet: '',
         byline: {original: ''},
     });
+    const [archiveFormData, setArchiveFormData] = useState({
+        month: 0,
+        year: 0,
+    });
+    const monthDict = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    };
     const API_KEY = process.env.REACT_APP_NYT_API_KEY;
-    const archiveURL = `https://corsproxy.io/?https://api.nytimes.com/svc/archive/v1/2024/2.json?api-key=${API_KEY}`;
+
+    // Handle when the form changes
+    const handleChange = (event) => {
+        setArchiveFormData({
+            ...archiveFormData,
+            [event.target.name]: event.target.value
+        });
+    };
 
     // Handle the fetch archive button.
     const fetchArchive = async (event) => {
+        const archiveURL = `https://corsproxy.io/?https://api.nytimes.com/svc/archive/v1/${archiveFormData.year}/${archiveFormData.month}.json?api-key=${API_KEY}`;
         event.preventDefault();
 
         const archiveResponse = await fetch(archiveURL);
@@ -21,27 +47,54 @@ function MainPage() {
             console.log(`***** Archive Data ==> ${JSON.stringify(archiveData)}`);
 
             setCopyright(archiveData.copyright);
-            setArticleLoaded(true);
+            setResultsLoaded(true);
             setMyArticle(archiveData.response.docs[0]);
-            console.log(`***** Copyright: ${copyright}`);
-            console.log(`***** Article Info: ${JSON.stringify(myArticle)}`);
-            console.log(`***** Component Status: ${articleLoaded}`);
-
         } else {
             window.confirm("There was a problem fetching the NY Times archive.")
         }
+        console.log(`***** Copyright: ${copyright}`);
+        console.log(`***** Article Info: ${JSON.stringify(myArticle)}`);
+        console.log(`***** Component Status: ${resultsLoaded}`);
     };
+
+    // Handle the search archive button.
+    // const searchArchive = async (event) => {
+    //     const searchURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${API_KEY}`;
+    //     event.preventDefault();
+
+    //     const archiveResponse = await fetch(searchURL);
+    //     if (archiveResponse.ok) {
+    //         const archiveData = await archiveResponse.json();
+    //         console.log(`***** Search Archive Data ==> ${JSON.stringify(archiveData)}`);
+
+    //     } else {
+    //         window.confirm("There was a problem searching the NY Times archive.")
+    //     }
+    // };
+
+    // Handle the Top Stories button.
+    // const fetchTopStories = async (event) => {
+    //     event.preventDefault();
+
+    //     const archiveResponse = await fetch(topStoriesURL);
+    //     if (archiveResponse.ok) {
+    //         const archiveData = await archiveResponse.json();
+    //         console.log(`***** Top Stories Data ==> ${JSON.stringify(archiveData)}`);
+
+    //     } else {
+    //         window.confirm("There was a problem fetching the NY Times Top Stories.")
+    //     }
+    // };
 
     const freshLanding = () => (
         <div className="initialdisplay">
-            <h1>Headline Editor Main Page</h1>
-            <button onClick={fetchArchive}>Fetch Archive</button>
+            <h2>No results yet.</h2>
         </div>
     );
 
     const archiveLoaded = () => (
         <div className="archivedisplay">
-            <h1>NY Times Archive for 2024 February</h1>
+            <h1>NY Times Archive for { archiveFormData.year } { monthDict[archiveFormData.month] }</h1>
             <h2>{copyright}</h2>
             <h2>{myArticle.headline.main}</h2>
             <p>{myArticle.snippet}</p>
@@ -51,7 +104,20 @@ function MainPage() {
 
     return (
         <div className="content">
-            {articleLoaded ? archiveLoaded() : freshLanding()}
+            <h1>Search the NY Times Archive</h1>
+            <div className="searches">
+                <form onSubmit={fetchArchive} id='archive-form' className='archive-form'>
+                    <label htmlFor="year">Year </label>
+                    <input onChange={handleChange} type="text" id="year" name="year" placeholder="2024" />
+                    <label htmlFor="month">Month </label>
+                    <input onChange={handleChange} type="text" id="month" name="month" placeholder="1" />
+                </form>
+                <button onClick={fetchArchive}>NY Times Archive</button>
+                <hr></hr>
+            </div>
+            <div className="results">
+                {resultsLoaded ? archiveLoaded() : freshLanding()}
+            </div>
         </div>
 
     );
