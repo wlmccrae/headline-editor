84ace45d399491fb33a04820218a4f9f56612c26
Wanda McCrae <wanda.mccrae@gmail.com>
2024-05-11 10:16:09 -0400

    Fetches and displays archive data

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage/>} />
       </Routes>
     </BrowserRouter>
   );
--- ghi/src/components/MainPage.js
+++ ghi/src/components/MainPage.js
@@ -1,50 +1,52 @@
 import React, {useState} from 'react';
 
 function MainPage() {
-    const API_KEY = process.env.NYT_API_KEY;
     const [copyright, setCopyright] = useState('');
     const [articleLoaded, setArticleLoaded] = useState(false);
     const [myArticle, setMyArticle] = useState({
-        headline: '',
+        headline: {main: ''},
         snippet: '',
-        byline: {},
+        byline: {original: ''},
     });
+    const API_KEY = process.env.REACT_APP_NYT_API_KEY;
+    const archiveURL = `https://corsproxy.io/?https://api.nytimes.com/svc/archive/v1/2024/2.json?api-key=${API_KEY}`;
 
-    const fetchArchive = async(event) => {
+    // Handle the fetch archive button.
+    const fetchArchive = async (event) => {
         event.preventDefault();
 
-        // Fetch the archive
-        const archiveURL = `https://api.nytimes.com/svc/archive/v1/2024/2.json?api-key=${API_KEY}`;
-        console.log(`***** NYT API KEY: ${API_KEY}`);
         const archiveResponse = await fetch(archiveURL);
-
         if (archiveResponse.ok) {
             const archiveData = await archiveResponse.json();
+            console.log(`***** Archive Data ==> ${JSON.stringify(archiveData)}`);
 
             setCopyright(archiveData.copyright);
-            setMyArticle(archiveData.docs[0]);
             setArticleLoaded(true);
+            setMyArticle(archiveData.response.docs[0]);
+            console.log(`***** Copyright: ${copyright}`);
+            console.log(`***** Article Info: ${JSON.stringify(myArticle)}`);
+            console.log(`***** Component Status: ${articleLoaded}`);
+
         } else {
             window.confirm("There was a problem fetching the NY Times archive.")
         }
-
     };
 
     const freshLanding = () => (
-        <>
+        <div className="initialdisplay">
             <h1>Headline Editor Main Page</h1>
-            <button onClick={fetchArchive}>Load Archive</button>
-        </>
+            <button onClick={fetchArchive}>Fetch Archive</button>
+        </div>
     );
 
     const archiveLoaded = () => (
-        <>
+        <div className="archivedisplay">
             <h1>NY Times Archive for 2024 February</h1>
             <h2>{copyright}</h2>
-            <h2>{myArticle.headline}</h2>
+            <h2>{myArticle.headline.main}</h2>
             <p>{myArticle.snippet}</p>
             <p>by {myArticle.byline.original}</p>
-        </>
+        </div>
     )
 
     return (
